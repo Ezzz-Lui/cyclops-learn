67bf641685175160f1ae51626aaba8b9e4cfa8e4
Ezzz-Lui <ogn.lui@gmail.com>
2026-08-29 11:25:02 -0600

    feat: added shacn components, convex lib and context for agent.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono, Figtree } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils";
+import { TooltipProvider } from "@/components/ui/tooltip"
 
 const figtree = Figtree({subsets:['latin'],variable:'--font-sans'})
 
@@ -23,7 +24,11 @@ export default function RootLayout({
       className={cn("antialiased", fontMono.variable, "font-sans", figtree.variable)}
     >
       <body>
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider>
+          <TooltipProvider>
+            {children}
+          </TooltipProvider>
+        </ThemeProvider>
       </body>
     </html>
   )
